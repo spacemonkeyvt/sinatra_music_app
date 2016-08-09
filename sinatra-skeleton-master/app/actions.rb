e7132b0ea005beb1a13aa0248496dad5fcd129b2
Vishal Tiwari <vishal.tiwari.09@gmail.com>
2016-08-09 17:04:32 -0400

    Gem 'bcrypt' included. `/signup/new.erb` path and view added. `get/post` methods included in `actions.rb` file to create a new user and save them to the database. `user.rb` model now includes secure password method. Front-page login fields included in `index.erb` without controller or model validation support.

--- sinatra-skeleton-master/app/actions.rb
+++ sinatra-skeleton-master/app/actions.rb
@@ -12,6 +12,10 @@ get '/songs/new' do
   erb :'songs/new'
 end
 
+get '/signup' do
+  erb :'signup/new'
+end
+
 post '/songs' do
   @song = Song.new(
     title: params[:title],
@@ -22,23 +26,43 @@ post '/songs' do
     redirect '/songs'
 end
 
+post '/songs' do
+  @user = User.new(
+    name: params[:name],
+    email: params[:email],
+    password: params[:password]
+    )
+  @user.save
+  redirect '/songs'
+end
+
+
+
 
 # [1] USERS CAN SIGNUP (email, password)
 #
 # I need to create a new user table:
-#   > with email and password columns
-#   > I do this by creating a new migration and migrating it
-#   > I would need to update the model with the relationship.
-#
-#   >
-
-
+#   x with email and password columns
+#   x I do this by creating a new migration and migrating it
+#   x I would need to update the model with the relationship
 
 # [2] USERS CAN LOGIN
+# I need to create a login page:
+#   x it needs to have fields for input
+#   > it needs to verify if the user has signed up
+#   > it needs to redirect to back with an error message
+#   > it needs to redirect to the songs list page if it works
+#   > it needs to have a session id that persists across all pages
 
 # [3] USERS CAN LOGOUT
+# I need to create a logout link:
+#   > clicking the link should end the user session
+#   > clicking the link should redirect them back to login page
+
 
 # [4] RECORD USER ADDING SONG/TRACK
+
+
 
 # [5] USERS CAN UPVOTE SONGS ONCE
 
--- sinatra-skeleton-master/app/models/user.rb
+++ sinatra-skeleton-master/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :songs
+  # We can't be too careful! Never store passwords in pain text!
+  # You need to install the bcrypt gem for this to work.
+  has_secure_password
+
+  has_many :songs, dependent: :destroy
+
 end
